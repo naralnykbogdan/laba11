36f4fb28a0884a4244418c07041b3eb8d567db90
Bogdan <59779536+naralnykbogdan@users.noreply.github.com>
2020-06-11 18:07:12 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,37 +1,30 @@
 #Дан двійковий файл f, компоненти якого є дійсними числами. Знайти суму,
 #добуток, суму квадратів і квадрат добутку компонентів файлу f, а також останню
 #компоненту файлу. Результати записати у файл g.
-sum_1 = 0
-p_1 = 1
-sum_2 = 0
-p_2 = 1
-count = 0
-q = 0
-with open("first.txt", 'rb') as f, open("second.txt", 'wb') as g:
-    for item in f:  
-        q -= 1
-        last_element = float(item) #формули
-        sum_1 += float(item)
-        p_1 *= float(item)
-        sum_2 += (float(item))
-        p_2 *= float(item)
-        a = p_2
-        count += 1
-with open("first.txt", 'rb') as f, open("second.txt", 'wb') as g:  
-    for item in f:  
-        s = "Сума чисел:" + str(sum_1) + "\n"            #конвертування цілого числа в строку
-        bt = s.encode()  
-        g.write(bt)  # записуєм у файл
-        s = "Сума квадратів чисел:" + str(sum_2) + "\n"  #конвертування цілого числа в строку
-        bt = s.encode()  
-        g.write(bt)  # записуєм у файл
-        s = "Добуток чисел:" + str(p_1) + "\n"            #конвертування цілого числа в строку
-        bt = s.encode()  
-        g.write(bt)  # записуєм у файл
-        s = "Квадрат добутку чисел:" + str(a) + "\n"      #конвертування цілого числа в строку
-        bt = s.encode() 
-        g.write(bt)  # записуєм у файл
-        s = "Останній елемент:" + str(last_element)  
-        bt = s.encode()  
-        g.write(bt)  # записуєм у файл
-        break
+sum,dob,sum_k,dob_k=0,1,0,1
+with open(r"C:\Users\Админ\Desktop\f1.txt", 'rb') as f, open('g1.txt', mode='wb') as g:
+        for i in f:
+                number = float(i)
+                sum += float(i)
+                dob *= float(i)
+                sum_k += (float(i))**2
+                dob_k = dob**2
+
+        a = "Сума чисел = " + str(sum) + "\n"  # конв-ня float в str
+        a1 = "Сума квадратів = " + str(sum_k) + "\n"
+        a2 = "Добуток = " + str(dob) + "\n"
+        a3 = "Квадрат добутку = " + str(dob_k) + "\n"
+        a4 = str(number)
+
+        b = a.encode() 
+        b1 = a1.encode()
+        b2 = a2.encode()
+        b3 = a3.encode()
+        b4 = a4.encode()
+
+        g.write(b)
+        g.write(b1)
+        g.write(b2)
+        g.write(b3)
+        g.write(b4)
+
